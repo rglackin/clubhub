8b846575a4487dff8ef02725fc4e862eb0a2b5af
Ruairí Glackin <ruairiglackin@gmail.com>
2024-02-21 20:48:19 +0000

    login class view made

--- clubhub/crm/urls.py
+++ clubhub/crm/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
 
     path('register', views.RegisterFormView.as_view(), name="register"),
 
-    path('my-login', views.my_login, name="my-login"),
-
-    path('dashboard', views.dashboard, name="dashboard"),
+    path('login', views.LoginView.as_view(), name="login"),
+    
+    path('logout', views.LogoutView.as_view(),name='logout'),
+   # path('dashboard', views.dashboard, name="dashboard"),
 
     path('user-logout', views.user_logout, name="user-logout"),
     
