b2fd4a9041034a0d965ee575bfa1075cc21fd571
oisinfrizzell26 <oisinfrizzell26@gmail.com>
2024-02-21 17:20:23 +0000

    adding validation

--- clubhub/crm/models.py
+++ clubhub/crm/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 """ deprecated
 class User(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True)         
@@ -21,9 +22,33 @@ class User(models.Model):
     is_admin = models.BooleanField(null=False, default = False)
     user_created = models.DateTimeField(auto_now_add=True)  
     user_updated = models.DateTimeField(auto_now_add=True) 
+
     class Meta:
         managed = False
         db_table = 'user'
+
+
+
+
+
+
+    def clean_username(self):
+        if len(self.username) > 20:
+            raise ValidationError("Username can only be 20 characters")
+        
+    def clean_password(self):
+        if len(self.password) < 7 or not any(char.isdigit() for char in self.password) or not any(char.isupper() for char in self.password):
+            raise ValidationError("Password must be at least 7 characters long and include a capital letter and a number")
+    
+    def clean_email(self):
+
+        if '@' not in self.email or not self.email.endswith('.com'):
+            raise ValidationError("Invalid email address")
+        
+    def clean_phone_no(self):
+        if not self.phone_no.isdigit() or len(self.phone_no) != 9:
+            raise ValidationError('Invalid phone number')
+
 
 class Club(models.Model):
     club_id = models.AutoField(primary_key=True)
