00495160353226f8da62fcc804347977704a11d9
Ruairí Glackin <ruairiglackin@gmail.com>
2024-02-22 16:59:02 +0000

    prevent access to dashboard if not logged in

--- clubhub/clubhub/settings.py
+++ clubhub/clubhub/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "crm.middleware.CheckLoggedIn"
 ]
 
 ROOT_URLCONF = "clubhub.urls"
