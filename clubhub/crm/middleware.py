8e2f2fec1daa69e26532de6f660d5ea8ba64710d
Ruairí Glackin <ruairiglackin@gmail.com>
2024-02-24 16:35:12 +0000

    admin dashboard made

--- clubhub/crm/middleware.py
+++ clubhub/crm/middleware.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import User
+
 
 class CheckLoggedIn:
     def __init__(self, get_response):
@@ -8,7 +10,14 @@ class CheckLoggedIn:
     def __call__(self, request):
         response = self.get_response(request)
         if request.path == reverse('crm:dashboard'):
-        #if user not logged in 
-            if not request.session.get('user_id'):
+        #if user not logged in or not approved
+            user_id = request.session.get('user_id')
+            approved = False
+            try:
+                user = User.objects.get(id = user_id)
+                approved = user.approved
+            except User.DoesNotExist:
+                pass
+            if not user_id or not approved :
                 return HttpResponseRedirect(reverse('crm:index'))
         return response
